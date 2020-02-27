c83428ae4241161f9249fbeb6fdbd556e9d329e2
Ania-M-Pienio <aniapienio@gmail.com>
2020-02-27 18:34:39 -0500

    setup Main

---
+++ src/components/MainContainer.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+class MainContainer extends Component {
+  // Main Constructor
+  constructor() {
+    super();
+    this.state = {
+      seasonChoice: '',
+      timeChoice: '',
+      colorChoice: '',
+    }
+  }
+
+  // Render 
+  render() {
+    return (
+      <main className="mainContainer">
+
+
+        
+      </main>
+    )
+  }
+}
