2fd0090c1f3d084eb769d3e6411a55f69fc7fc7e
Ania-M-Pienio <aniapienio@gmail.com>
2020-02-27 18:17:07 -0500

    icons imported

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
-import bm8 from './temp'
 import "./App.scss";
 
 class App extends Component {
   render() {
-    const myJSON = JSON.stringify(bm8);
-    console.log(myJSON);
-    return <div className="App">
-      <h1>App Hooked Up</h1>
-    </div>;
+    return (
+      <div className="App">
+        <h1>App Hooked Up</h1>
+        <i className="material-icons md-60">face</i>
+      </div>
+    );
   }
 }
 
