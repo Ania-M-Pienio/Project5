cfc7e0670d69cc06231ed156bc11d4b0c542ed02
Ania-M-Pienio <aniapienio@gmail.com>
2020-04-16 14:19:37 -0400

    review

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -8,28 +8,12 @@ function Splash(props) {
         <div className="splashLogo">
           <Logo />
         </div>
-        <h1> GET IDEAL WEDDING COLOURS </h1>
-        <ul>
-          <li>
-            [<span> {"1"} </span> {"] invites    ["} <span> {"2"} </span>{" "}
-            {"] floral "}
-          </li>
-          <li>
-            [<span> {"3"} </span> {"] center pieces    ["} <span> {"4"} </span>{" "}
-            {"] table-ware "}
-          </li>
-          <li>
-            [<span> {"5"} </span> {"] venues   ["} <span> {"6"} </span>{" "}
-            {"] cakes "}
-          </li>
-          <li>
-            [<span> {"7"} </span> {"] favors   ["} <span> {"8"} </span>{" "}
-            {"] patterns "}
-          </li>
-        </ul>
-        <button type="button" onClick={props.onStart}>
-          GET STARTED
-        </button>
+          <button type="button" onClick={props.onStart}>
+            GET STARTED
+          </button>
+ 
+          <h1> GET IDEAL WEDDING COLOURS </h1>
+
       </section>
     );
 }
