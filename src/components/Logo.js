86f6c114f1b284d1e6f96fed69fcc97b44479e26
Ania-M-Pienio <aniapienio@gmail.com>
2020-04-21 08:39:00 -0400

    logo animation

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,24 +1,35 @@
 import React from "react";
 import m8 from "../assets/BM8B.png";
-// import leaf from "../assets/leaf.png";
+import leafLV from "../assets/leaf-lv.png";
 import leafRV from "../assets/leaf-rv.png";
+import leafC from "../assets/leaf-c.png";
 
 function Logo() {
   return (
     <div className="logo">
       <div className="logoBorder">
-        <div className="leafOne">
-          <img
-            src={leafRV}
-            className="leaf1"
-            alt="bride"
-          ></img>
+        <div className="leaf leafLeft">
+          <img src={leafRV} className="leafImage leafR" alt="bride"></img>
+        </div>
+        <div className="leaf leafLeftBottom">
+          <img src={leafRV} className="leafImage leafR" alt="bride"></img>
+        </div>
+        <div className="leaf leafCenter">
+          <img src={leafC} className="leafImage leafC" alt="bride"></img>
         </div>
         <img src={m8} alt="Bride Mate Logo"></img>
+        <div className="leaf leafCenterBottom">
+          <img src={leafC} className="leafImage leafC" alt="bride"></img>
+        </div>
+        <div className="leaf leafRight">
+          <img src={leafLV} className="leafImage leafL" alt="bride"></img>
+        </div>
+        <div className="leaf leafRightBottom">
+          <img src={leafLV} className="leafImage leafL" alt="bride"></img>
+        </div>
       </div>
     </div>
   );
 }
 
 export default Logo;
-
