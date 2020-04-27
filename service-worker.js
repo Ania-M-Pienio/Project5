727cba3ce0eaf2078c0dbafb5afa1c4e05cda367
Ania-M-Pienio <aniapienio@gmail.com>
2020-04-27 13:37:25 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Project5/precache-manifest.178045b21dac7fbe459ae3fbf371ddb9.js"
+  "/Project5/precache-manifest.7e34ec2c5607229e71ed796852cc56dc.js"
 );
 
 self.addEventListener('message', (event) => {
